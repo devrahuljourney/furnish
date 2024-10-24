58068a717b5799f3d4022eeae8db594d9e6d398e
rahulkumarv269 <rahulkumarv269@gmail.com>
2024-10-24 14:57:40 +0530

    change base url

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,5 @@
 // const BASE_URL  =  "http://localhost:4000/api/v1"
-const BASE_URL  =  "https://furnish-backend.vercel.app/api/v1"
+const BASE_URL  =  "https://ranbankabackend.vercel.app/api/v1"
 
 
 
